f53b229eb7d81973c8392c16f4e21e927c4b934f
Jagath Kumar Reddy <jagathkumarreddykatamareddy@gmail.com>
2025-05-15 20:22:59 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -3,8 +3,15 @@ import numpy as np
 
 # update/add code below ...
 def ways(n):
-    return None
+    max_5 = n//5
+    n_ways = 0
+    for i in len(max_5):
+        if n>(i*5):
+            n_ways+=1
+    
+    return n_ways
 
 
 def ways_np(n):
+    
     return None
